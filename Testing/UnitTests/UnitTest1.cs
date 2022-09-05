dd6d3140be45cab1e860e169cbd9c64fbec7dad3
Jim Nardecchia <jnardecchia@outlook.com>
2022-09-05 13:51:32 -0400

    implemented weighted randomized base stats

--- Testing/UnitTests/UnitTest1.cs
+++ Testing/UnitTests/UnitTest1.cs
@@ -1,10 +1,11 @@
 namespace UnitTests;
 using OgreBattleSagaCharacterBuilder.Data.TacticsOgre;
+using OgreBattleSagaCharacterBuilder.Domain.TacticsOgre;
 
 public class UnitTest1
 {
     [Fact]
-    public void LoadJsonData()
+    public void LoadJsonDataTest()
     {
         // Arrange
         var tacticsOgreDataService = new TacticsOgreDataService();
@@ -14,5 +15,24 @@ public class UnitTest1
 
         // Assert
         Assert.Equal(12, classes.Count);
+    }
+
+    [Fact]
+    public void RandomizeBaseStatsTest()
+    {
+        // Arrange
+        var tacticsOgreDataService = new TacticsOgreDataService();        
+        var baseStatsGenerations = new List<Statistics>();
+
+        // Act
+        for(var x = 0; x < 1000; x++)
+        {
+            baseStatsGenerations.Add(tacticsOgreDataService.GenerateBaseStats(Gender.Female));        
+        }
+                
+        // Assert        
+        Assert.InRange<decimal>(baseStatsGenerations.Count(x => x.HP == 57) / 1000M, .175M, .225M);
+        Assert.InRange<decimal>(baseStatsGenerations.Count(x => x.HP == 58) / 1000M, .55M, .65M);
+        Assert.InRange<decimal>(baseStatsGenerations.Count(x => x.HP == 59) / 1000M, .175M, .225M);
     }
 }
