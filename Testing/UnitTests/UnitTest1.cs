d42cb9ca0213258a862b4b8609b782dedfeb1daf
Jim Nardecchia <jnardecchia@outlook.com>
2022-09-06 08:21:56 -0400

    Added Base Stat randomizer test

--- Testing/UnitTests/UnitTest1.cs
+++ Testing/UnitTests/UnitTest1.cs
@@ -22,17 +22,24 @@ public class UnitTest1
     {
         // Arrange
         var tacticsOgreDataService = new TacticsOgreDataService();        
-        var baseStatsGenerations = new List<Statistics>();
+        var baseStatsGenerationsFemale = new List<Statistics>();
+        var baseStatsGenerationsMale = new List<Statistics>();
+        var numOfGenerations = 10000M;
 
         // Act
-        for(var x = 0; x < 1000; x++)
+        for(var x = 0; x < numOfGenerations; x++)
         {
-            baseStatsGenerations.Add(tacticsOgreDataService.GenerateBaseStats(Gender.Female));        
+            baseStatsGenerationsFemale.Add(tacticsOgreDataService.GenerateBaseStats(Gender.Female));
+            baseStatsGenerationsMale.Add(tacticsOgreDataService.GenerateBaseStats(Gender.Male));
         }
                 
         // Assert        
-        Assert.InRange<decimal>(baseStatsGenerations.Count(x => x.HP == 57) / 1000M, .175M, .225M);
-        Assert.InRange<decimal>(baseStatsGenerations.Count(x => x.HP == 58) / 1000M, .55M, .65M);
-        Assert.InRange<decimal>(baseStatsGenerations.Count(x => x.HP == 59) / 1000M, .175M, .225M);
+        Assert.InRange<decimal>(baseStatsGenerationsFemale.Count(x => x.HP == 57) / numOfGenerations, .175M, .225M);
+        Assert.InRange<decimal>(baseStatsGenerationsFemale.Count(x => x.HP == 58) / numOfGenerations, .55M, .65M);
+        Assert.InRange<decimal>(baseStatsGenerationsFemale.Count(x => x.HP == 59) / numOfGenerations, .175M, .225M);
+        Assert.InRange<decimal>(baseStatsGenerationsMale.Count(x => x.HP == 59) / numOfGenerations, .125M, .1725M);
+        Assert.InRange<decimal>(baseStatsGenerationsMale.Count(x => x.HP == 60) / numOfGenerations, .55M, .65M);
+        Assert.InRange<decimal>(baseStatsGenerationsMale.Count(x => x.HP == 61) / numOfGenerations, .175M, .225M);
+        Assert.InRange<decimal>(baseStatsGenerationsMale.Count(x => x.HP == 62) / numOfGenerations, .0375M, .0725M);
     }
 }
